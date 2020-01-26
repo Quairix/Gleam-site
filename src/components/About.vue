0bcd2873d00298835857ca93e8ddd8c490c547e4
Dmitriy <quarix@ya.ru>
2020-01-27 02:41:23 +0300

    Таблица оборудование и данные показателей в ней

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,154 +1,44 @@
 <template>
-<<<<<<< Updated upstream
-  <div>
-    <h1>Полигон для ядерных испытаний</h1>
-
-    <line-chart :chart-data="filldata"></line-chart>
-=======
-	<div>
-		<h1>Статистика нагрузки на сеть</h1>
-
-		<div style="width:500px; margin:auto">
-			<line-chart :chart-data="filldata"></line-chart>
-		</div>
-		<div class="row col-lg-4 col-md-4 col-sm-6" style="margin: auto" >
-			<label style="padding-top:7px" >День:</label>
-			<input style="margin: 0px 10px; width:120px" v-model="day" type="date" />
-			<label style="padding-top:7px">Результат:</label>
-			<input style="margin: 0px 10px; width:50px" v-model="dayValue" type="number" />
-		</div>
-		<div class="row">
-			<button style="width:160px; height:50px; padding:0" class="main-form-btn" @click="add()">Добавить/обновить</button>
-			<button style="margin-left:10px; width:100px; padding:0; height:50px" class="main-form-btn" @click="deleteDay()">Удалить</button>			
-		</div>
-	</div>
-</template>
-
-<script>
-import Vue from "vue";
-import VueChart from "vue-chartjs";
-import LineChart from "./LineChart.js";
-import { chartRef } from "../config/firebaseConfig";
-Vue.use(VueChart);
-export default {
-	
-	components: {
-		LineChart
-	},
-	data() {
-		this.load();
-		return {
-			day: "",
-			dayValue: "",
-			dayData: []
-		};
-	},
-	computed: {
-		filldata() {
-			let labels = [];
-			let data = [];
-			let list = [];
-			for (let m of this.dayData) {
-				list.push({ label: m.label, value: m.value });
-			}
-			list.sort(function(a, b) {
-				if (a.label > b.label) {
-					return 1;
-				}
-				if (b.label > a.label) {
-					return -1;
-				}
-				return 0;
-			});
-			for (let d of list) {
-				labels.push(d.label);
-				data.push(d.value);
-			}
-			return {
-				labels,
-				datasets: [
-				{
-					type:'bar',
-					label: "Статистика нагрузки на сеть за сутки",
-					backgroundColor:'rgba(139, 0, 0, 0.9)',
-					hoverBackgroundColor:'rgba(210, 105, 30, 0.9)',
-					data: data
-				}
-				]
-			};
-		}
-	},
-	methods: {
-		load() {
-			this.dayData = [];
-			chartRef.once("value", snapshot => {
-				snapshot.forEach(o => {
-					this.dayData.push({
-						key: o.key,
-						label: o.val().day,
-						value: o.val().dayValue
-					});
-				});
-			});
-		},
-		add() {
-			if (this.day != "") {
-				let doAdd = true;
-				this.dayData.forEach(o => {
-					if (o.label == this.day) {
-						this.update(o.key, o.label, this.dayValue);
-						doAdd = false;
-            //TODO: сделать break
-        }
-    });
-				if (doAdd) {
-					chartRef.push({ day: this.day, dayValue: this.dayValue });
-				}
-				this.load();
-				this.day = "";
-				this.dayValue = "";
-			}
-		},
-		update(key, dayString, dayNewValue) {
-			const edit = chartRef.child(key);
-			edit.update({ day: dayString, dayValue: dayNewValue });
-		},
-		deleteDay() {
-			if (this.day != "") {
-				this.dayData.forEach(o => {
-					if (o.label == this.day) {
-						const edit = chartRef.child(o.key);
-						edit.remove();
-            //TODO: сделать break
-        }
-    });
-				this.load();
-				this.day = "";
-				this.dayValue = "";
-			}
-		}
-	}
-};
-</script>
->>>>>>> Stashed changes
-
-    <h3 style="text-align: center">Ввод данных показателей</h3>
-    <p>
-      <label>День:</label>
-      <input v-model="day" type="text" />
-    </p>
-    <p>
-      <label>Результат:</label>
-      <input v-model="dayValue" type="number" />
-    </p>
-
-<<<<<<< Updated upstream
-    <p>
-      <button @click="add()">Добавить</button>
-    </p>
-    <p>
-      <button @click="deleteDay()">Удалить</button>
-    </p>
+  <div style="text-align:center">
+    <h1>Статистика нагрузки на сеть</h1>
+    <div class="row text-center" v-if="!isLoaded">Загрузка...</div>
+    <div class="row" v-if="isLoaded">
+      <div class="col-lg-7" style="width:60%; margin:auto">
+        <line-chart :chart-data="filldata"></line-chart>
+      </div>
+      <div class="row col-lg-3 col-md-2 col-sm-6" style="align-self: flex-start;">
+        <div class="row" style="align-self: flex-start;">
+          <div style="padding:7px 0 45px 0;">
+            <h5 style="display:inline-block">Оборудование:</h5>
+            <select class="form-control" @change="onChange($event)">
+              <option v-for="d in devices" :key="d" :value="d">{{d}}</option>
+            </select>
+          </div>
+        </div>
+        <div class="row" style="align-self: flex-start;">
+          <p>
+            <label>День:</label>
+            <input style="width:120px" v-model="day" type="date" />
+          </p>
+          <p style="padding-top:7px">
+            <label>Результат:</label>
+            <input style="width:50px" v-model="dayValue" type="number" />
+          </p>
+        </div>
+        <div class="row" style="align-self: flex-start;">
+          <button
+            style="width:160px; height:50px; padding:0"
+            class="main-form-btn"
+            @click="add()"
+          >Добавить/обновить</button>
+          <button
+            style="margin-left:10px; width:100px; padding:0; height:50px"
+            class="main-form-btn"
+            @click="deleteDay()"
+          >Удалить</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -156,21 +46,25 @@ export default {
 import Vue from "vue";
 import VueChart from "vue-chartjs";
 import LineChart from "./LineChart.js";
-import { chartRef } from "../config/firebaseConfig";
-
+import { db, devicesRef } from "../config/firebaseConfig";
 Vue.use(VueChart);
-
 export default {
   components: {
     LineChart
   },
   data() {
-    this.load();
+    this.load(db.ref("devices/device1"));
     return {
+      isLoaded: false,
       day: "",
       dayValue: "",
-      dayData: []
+      dayData: [],
+      devices: [],
+      chartRef: db.ref("devices/device1")
     };
+  },
+  mounted() {
+    this.getDevices();
   },
   computed: {
     filldata() {
@@ -197,10 +91,10 @@ export default {
         labels,
         datasets: [
           {
+            type: "bar",
             label: "Статистика нагрузки на сеть за сутки",
-            pointBackgroundColor: "#FF4dfa",
-            borderColor: "#36fa6a",
-            //backgroundColor:"#364dfa",
+            backgroundColor: "rgba(139, 0, 0, 0.9)",
+            hoverBackgroundColor: "rgba(139, 0, 0, 0.6)",
             data: data
           }
         ]
@@ -208,9 +102,25 @@ export default {
     }
   },
   methods: {
-    load() {
+    getDevices() {
+      let devices = [];
+      devicesRef.once("value", s => {
+        s.forEach(o => {
+          devices.push(o.key);
+        });
+      });
+      this.isLoaded = true;
+      //this.load(this.devices.find(x => x !== undefined));
+      this.devices = devices;
+      //console.log(this.devices.find(x => x !== undefined));
+    },
+    onChange(ref) {
+      this.chartRef = ref.target.value;
+      this.load(db.ref("devices/" + this.chartRef));
+    },
+    load(ref) {
       this.dayData = [];
-      chartRef.once("value", snapshot => {
+      ref.once("value", snapshot => {
         snapshot.forEach(o => {
           this.dayData.push({
             key: o.key,
@@ -231,27 +141,27 @@ export default {
           }
         });
         if (doAdd) {
-          chartRef.push({ day: this.day, dayValue: this.dayValue });
+          db.ref("devices/" + this.chartRef).push({ day: this.day, dayValue: this.dayValue });
         }
-        this.load();
+        this.load(db.ref("devices/" + this.chartRef));
         this.day = "";
         this.dayValue = "";
       }
     },
     update(key, dayString, dayNewValue) {
-      const edit = chartRef.child(key);
+      const edit = db.ref("devices/" + this.chartRef).child(key);
       edit.update({ day: dayString, dayValue: dayNewValue });
     },
     deleteDay() {
       if (this.day != "") {
         this.dayData.forEach(o => {
           if (o.label == this.day) {
-            const edit = chartRef.child(o.key);
+            const edit = db.ref("devices/" + this.chartRef).child(o.key);
             edit.remove();
             //TODO: сделать break
           }
         });
-        this.load();
+        this.load(db.ref("devices/" + this.chartRef));
         this.day = "";
         this.dayValue = "";
       }
@@ -259,8 +169,3 @@ export default {
   }
 };
 </script>
-<style>
-</style>
-=======
-</style>
->>>>>>> Stashed changes
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,26 +2,24 @@
   <div>
     <div class="main__intro">
       <p
-      class="main__info-text"
+        class="main__info-text"
       >AnMoNet - современная система для анализа вашей корпоративной вычислительной локальной сети. Она собирает данные, анализирует их и предоставляет службе администратора план по модернизации сети или ее участка.</p>
       <img
-      class="main__img"
-      src="https://www.networkmedia.com.hk/images/Product/Apcon/14078_Diagram_IntellaStore_II_Applications.png"
-      alt
+        class="main__img"
+        src="https://www.networkmedia.com.hk/images/Product/Apcon/14078_Diagram_IntellaStore_II_Applications.png"
+        alt
       />
     </div>
     <div class="main__info">
       <img
-      class="main__info-img"
-      src="https://tools.ru/catalog_files/category_10968/images/nG1-Grid.png"
+        class="main__info-img"
+        src="https://tools.ru/catalog_files/category_10968/images/nG1-Grid.png"
       />
       <p
-      class="main__info-text"
+        class="main__info-text"
       >Система не имеет аналогов за счет формирования плана модернизации, который учитывает слабые места вашей сети и предлагает решение по их уcтранению</p>
     </div>
   </div>
-
-
 </template>
 
 <style>
@@ -44,5 +42,4 @@
   max-height: 200px;
   margin: 150px auto;
 }
-
 </style>
